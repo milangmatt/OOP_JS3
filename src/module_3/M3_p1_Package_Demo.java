9ce48814964b6720f88afc80d5c07f018ce45659
Milangm <milangeorgem@gmail.com>
2023-12-21 13:45:14 +0530

    Module_3_p1

---
+++ src/module_3/M3_p1_Package_Demo.java
@@ -0,0 +1,7 @@
+package module_3; // this is the package declaration statement
+
+public class M3_p1_Package_Demo {
+    public static void main(String[] args) {
+        System.out.println("This is a Example program to demonstrate packages in java");
+    }
+}
